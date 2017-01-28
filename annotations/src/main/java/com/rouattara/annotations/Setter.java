cd148c97fbdcdb5e7ab7fd39507b8adebb4ba5df
Ouattara Romuald <ouattararomuald@yahoo.fr>
2017-01-28 17:55:02 +0100

    Add Annotations classes

---
+++ annotations/src/main/java/com/rouattara/annotations/Setter.java
@@ -0,0 +1,11 @@
+package com.rouattara.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.SOURCE)
+@Target({ ElementType.TYPE, ElementType.CONSTRUCTOR, ElementType.METHOD })
+public @interface Setter {
+}
---
+++ annotations/src/main/java/com/rouattara/annotations/Version.java
@@ -0,0 +1,14 @@
+package com.rouattara.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.SOURCE)
+@Target({ ElementType.TYPE, ElementType.CONSTRUCTOR, ElementType.METHOD })
+public @interface Version {
+  int major();
+
+  int minor();
+}
